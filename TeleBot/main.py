8f7c1a4cc736bf8d65f1dbd32bc6134007ae763d
Vladislav Golyakin <119048112+V-V-G-professional@users.noreply.github.com>
2023-11-21 19:10:45 +0300

    Update main.py

--- TeleBot/main.py
+++ TeleBot/main.py
@@ -1,11 +1,15 @@
 import requests
 import json
 from http import HTTPStatus
+
+from telebot import types
+
 from tokens import API, bot
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!\nНапиши название города, что бы узнать погоду.')
+
 
 @bot.message_handler(content_types=['text'])
 def get_weather(message):
@@ -14,13 +18,20 @@ def get_weather(message):
     if res.status_code == HTTPStatus.OK:
         data = json.loads(res.text)
         temp = data["main"]["temp"]
-        image = 'hot.png' if temp > 5.0 else 'temperature.png'
+        feels_like = data["main"]["feels_like"]
+        humidity = data["main"]["humidity"]
+        wind_speed = data["wind"]["speed"]
+        image = 'hot.png' if temp > 5.0 else 'cold.png'
         file = open('images/' + image, 'rb')
-        bot.send_message(message.chat.id, f'Сейчас погода: ' + '{:.1f}'.format(temp) + 'C')
+
+        
+        bot.send_message(message.chat.id, f'Сейчас на улице: {"{:.1f}".format(temp)}C\nОщущается как: {"{:.1f}".format(feels_like)}С\nВлажность: {humidity}%\nСкорость ветра: {wind_speed}м/с')
         bot.send_photo(message.chat.id, file)
+
+
     else:
         bot.send_message(message.chat.id, 'Вы ввели некорректное название города.\nПопробуйте еще раз.')
 
 
-bot.polling(none_stop=True)
 
+bot.polling(none_stop=True)
